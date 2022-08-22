4f1a6b6d11ce8a5baa09b79235ee2e776b6c9be6
nataliekarri <nataliekarri@gmail.com>
2022-08-22 14:27:09 +0300

    add homework Seminar3

---
+++ Seminar3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 19.Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+    // Console.Write("Введите пятизначное число: ");
+    // int num = Convert.ToInt32(Console.ReadLine());
+    // int first = num/10000;
+    // int second = num/1000%10;
+
+    // if (num < 10000 || num > 99999)
+    // {
+    //   Console.Write("Это число не пятизначное");
+    // }
+    // else
+    // {
+    //   if ((second * 10 + first) == num%100)
+    //   {
+    //     Console.Write("Это палиндром");
+    //   }
+    //   else
+    //   {
+    //     Console.Write("Это не палиндром");
+    //   }
+    // }
+
+    // Задача 21.Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+    // Console.Write("Введите X1: ");
+    // int x1 = int.Parse(Console.ReadLine());
+    // Console.Write("Введите Y1: ");
+    // int y1 = int.Parse(Console.ReadLine());
+    // Console.Write("Введите Z1: ");
+    // int z1 = int.Parse(Console.ReadLine());
+    // Console.Write("Введите X2: ");
+    // int x2 = int.Parse(Console.ReadLine());
+    // Console.Write("Введите Y2: ");
+    // int y2 = int.Parse(Console.ReadLine());
+    // Console.Write("Введите Z2: ");
+    // int z2 = int.Parse(Console.ReadLine());
+
+    // double distance = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+
+    // Console.WriteLine($"Расстояние = {distance:f2}");
+
+    //Задача 23.Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+    // Console.Write("Введите число: ");
+    // int num = Convert.ToInt32(Console.ReadLine());
+    // int i = 1;
+
+    // while (i<=num)
+    // {
+    //   Console.Write(Math.Pow(i,3) + ",");
+    //   i++;
+    // }
