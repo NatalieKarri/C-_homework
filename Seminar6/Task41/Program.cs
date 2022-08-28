57f50998023851c3f24a8a4aa64cde41a295546f
nataliekarri <nataliekarri@gmail.com>
2022-08-28 11:18:44 +0300

    add task43

--- Seminar6/Task41/Program.cs
+++ Seminar6/Task41/Program.cs
@@ -2,12 +2,12 @@
 
     int[] CreateArray()
       {
-      Console.Write("Введите количество элементов массива");
+      Console.Write("Введите количество элементов массива: ");
       int size = Convert.ToInt32(Console.ReadLine());
       int[] array = new int[size];
       for(int i = 0; i < size; i++)
         {
-        Console.WriteLine($"Введите {i+1} элемент массива");
+        Console.WriteLine($"Введите {i+1} элемент массива: ");
         array[i] = Convert.ToInt32(Console.ReadLine());
         }
         return array;
---
+++ Seminar6/Task43/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+    Console.Write("Введите k1: ");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите b1: ");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+    double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+    
+    double x = 0;
+    double y = 0;
+
+if(k1 == k2)
+{
+  Console.Write("Прямые не пересекаются");
+}
+else
+{
+  x = (b2 - b1)/(k1 - k2);
+  y = k1 * x + b1;
+  Console.Write($"{x:f1},{y:f1}");
+}
