d927931f215220c556c546d5755abafeabe425b7
nataliekarri <nataliekarri@gmail.com>
2022-08-28 11:29:39 +0300

    add task42

---
+++ Seminar6/Task42/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 42. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+    
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string result = "";
+    while(num > 0)
+    {
+      result += num%2;
+      num =  num/2;
+    }
+    
+char[] inputarray = result.ToCharArray();
+Array.Reverse(inputarray);
+string output = new string(inputarray);
+
+
+Console.WriteLine($"В двоичной системе = {output}");
