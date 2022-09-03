6adb5cfbf093471cbc60c9cab7fb2e6d0717db32
nataliekarri <nataliekarri@gmail.com>
2022-09-03 16:09:00 +0300

    add Seminar8

---
+++ Seminar8/Task54/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задача 54: Задайте двумерный массив.
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+int[,] FillArray(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Sorting(int[,] matrix) //сортировка пузырьком
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int n = 0; n < matrix.GetLength(1) - 1; n++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    int tmp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = tmp;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] array = FillArray(row, column);
+PrintArray(array);
+Sorting(array);
+System.Console.WriteLine();
+PrintArray(array);
---
+++ Seminar8/Task60/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 60.Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+bool CheckUnique (int[] array, int number)
+{
+bool check =true;
+for (int i = 0; i < array.Length; i++)
+{
+    if (number == array[i])
+    {
+        check = false;
+    }
+}
+return check;
+}
+
+
+
+int[,,] FillArray(int rows, int columns, int depth)
+{
+    int[,,] cube = new int[rows, columns, depth];
+    int[] array = new int[rows*columns*depth];
+    int n =0;
+    for (int i = 0; i < cube.GetLength(0); i++)
+    {
+        for (int j = 0; j < cube.GetLength(1); j++)
+        {
+            for (int k = 0; k < cube.GetLength(2); k++)
+            {
+                while(CheckUnique(array, cube[i,j,k]) == false)
+                {
+                    cube[i, j, k] = new Random().Next(10, 100);
+                }
+                array[n] = cube[i,j,k];
+                n++;
+            }
+            
+        }
+    }
+    return cube;
+}
+
+void PrintArray(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]} ({i},{j},{k})\t");
+            } 
+        Console.WriteLine();
+        }
+        
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите глубину: ");
+int depth = Convert.ToInt32(Console.ReadLine());
+int[,,] array = FillArray(row, column, depth);
+PrintArray(array);
